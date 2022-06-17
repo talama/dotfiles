88544e8fa85f6266830de1e9080b3ca2cfd60978
talama <talama@protonmail.com>
2022-06-17 23:54:44 +0200

    Updated options

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -38,7 +38,7 @@ vim.opt.termguicolors = true
 
 -- Sidebar
 vim.o.number = true -- line number on the left
-vim.o.numberwidth = 3 -- always reserve 3 spaces for line number
+vim.o.numberwidth = 4 -- always reserve 3 spaces for line number
 vim.o.modelines = 0
 vim.o.showcmd = true -- display command in bottom bar
 
