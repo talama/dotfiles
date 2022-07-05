0bd70deedd42bd1d329d64e45a5583db3965c878
talama <talama@protonmail.com>
2022-07-05 18:19:51 +0200

    Updated terminal orientation

--- config/nvim/lua/talama/plugins/toggleterm.lua
+++ config/nvim/lua/talama/plugins/toggleterm.lua
@@ -35,7 +35,7 @@ toggleterm.setup({
   start_in_insert = true,
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   persist_size = true,
-  direction = 'float', -- | 'horizontal' | 'window' | 'float',
+  direction = 'horizontal', -- | 'horizontal' | 'window' | 'float',
   close_on_exit = true, -- close the terminal window when the process exits
   shell = vim.o.shell, -- change the default shell
   -- This field is only relevant if direction is set to 'float'
