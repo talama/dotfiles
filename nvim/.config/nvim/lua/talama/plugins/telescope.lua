d7d0e9e148f2ac978efe366dc01f50cbcc0b03e8
talama <talama@protonmail.com>
2022-07-09 16:01:46 +0200

    Updated telescope

--- nvim/.config/nvim/lua/talama/plugins/telescope.lua
+++ nvim/.config/nvim/lua/talama/plugins/telescope.lua
@@ -3,10 +3,12 @@ if not status_ok then
   return
 end
 
-local actions = require('telescope.actions')
-
 telescope.load_extension('media_files')
 telescope.load_extension('fzf')
+telescope.load_extension("ui-select")
+telescope.load_extension("live_grep_args")
+
+local actions = require('telescope.actions')
 
 local git_icons = {
   added = "  ",
@@ -69,7 +71,7 @@ telescope.setup({
   pickers = {
     find_files = {
       theme = "dropdown",
-      previewer = false,
+      previewer = true,
       -- find_command = { "find", "-type", "f" },
       -- find_command = { "fd", "-H", "-I" }, -- "-H" search hidden files, "-I" do not respect to gitignore
     },
@@ -83,11 +85,34 @@ telescope.setup({
     -- builtin picker
   },
   extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+
+    -- fzf syntax
+    -- Token	Match type	Description
+    -- sbtrkt	fuzzy-match	Items that match sbtrkt
+    -- 'wild'	exact-match (quoted)	Items that include wild
+    -- ^music	prefix-exact-match	Items that start with music
+    -- .mp3$	suffix-exact-match	Items that end with .mp3
+    -- !fire	inverse-exact-match	Items that do not include fire
+    -- !^music	inverse-prefix-exact-match	Items that do not start with music
+    -- !.mp3$	inverse-suffix-exact-match	Items that do not end with .mp3
     fzf = {
       fuzzy = true, -- false will only do exact matching
       override_generic_sorter = true, -- override the generic sorter
       override_file_sorter = true, -- override the file sorter
       case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    },
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown({
+        -- even more opts
+      }),
+    },
+    live_grep_args = {
+      auto_quoting = false, -- enable/disable auto-quoting
     },
     media_files = {
       -- filetypes whitelist
@@ -95,5 +120,10 @@ telescope.setup({
       filetypes = { "png", "webp", "jpg", "jpeg" },
       find_cmd = "rg", -- find command (defaults to `fd`)
     },
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
   },
 })
