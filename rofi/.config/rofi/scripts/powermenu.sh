5298e5be5656628fb9754d0fc4d41fc6f4c6ff89
talama <talama@protonmail.com>
2022-07-16 18:46:53 +0200

    Updated power menu

---
+++ rofi/.config/rofi/scripts/powermenu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$@" ]; then
+    echo -en "[ Suspend]\n"
+    echo -en "[ Lock]\n"
+    echo -en "[ Logout]\n"
+    echo -en "[ Hibernate]\n"
+    echo -en "[ Reboot]\n"
+    echo -en "[ Shutdown]\n"
+else
+    if [ "$1" = "Shutdown" ]; then
+        sudo shutdown now
+    elif [ "$1" = "Logout" ]; then
+        i3-msg exit
+    elif [ "$1" = "Reboot" ]; then
+        sudo reboot
+    elif [ "$1" = "Suspend" ]; then
+        systemctl suspend
+    elif [ "$1" = "Lock" ]; then
+        i3lock --nofork
+        #sudo ~/.local/bin/betterlockscreen -l blur
+    elif [ "$1" = "Hibernate" ]; then
+        sudo systemctl hibernate
+    fi
+fi
