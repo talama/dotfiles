e5f02d97101bd87a9af1499a46a5380d9864658b
talama <talama@protonmail.com>
2022-07-19 18:58:38 +0200

    Updated telescope config

--- nvim/.config/nvim/lua/talama/plugins.lua
+++ nvim/.config/nvim/lua/talama/plugins.lua
@@ -1,133 +1,141 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- Install your plugins here
-return require('packer').startup(function(use)
-  --  My plugins here
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	--  My plugins here
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- Needed to load first
-  use { 'lewis6991/impatient.nvim' }
-  -- use { 'nathom/filetype.nvim' }
-  use { 'nvim-lua/plenary.nvim' }
-  use { 'kyazdani42/nvim-web-devicons' }
-  use { 'goolord/alpha-nvim', config = "require('talama.plugins.alpha')" }
-  use("antoinemadec/FixCursorHold.nvim")
+	-- Needed to load first
+	use({ "lewis6991/impatient.nvim" })
+	-- use { 'nathom/filetype.nvim' }
+	use({ "nvim-lua/plenary.nvim" })
+	use({ "kyazdani42/nvim-web-devicons" })
+	use({ "goolord/alpha-nvim", config = "require('talama.plugins.alpha')" })
+	use("antoinemadec/FixCursorHold.nvim")
 
-  -- Colorschemes
-  use { 'ful1e5/onedark.nvim' }
+	-- Colorschemes
+	use({ "ful1e5/onedark.nvim" })
 
-  -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', config = "require('talama.plugins.treesitter')" }
-  use { 'nvim-treesitter/nvim-treesitter-textobjects', after = { 'nvim-treesitter' } }
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", config = "require('talama.plugins.treesitter')" })
+	use({ "nvim-treesitter/nvim-treesitter-textobjects", after = { "nvim-treesitter" } })
 
-  -- Telescope
-  use { "nvim-telescope/telescope-media-files.nvim" }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use { 'nvim-telescope/telescope.nvim',
-    config = "require('talama.plugins.telescope')",
-    requires = {
-      { 'nvim-lua/popup.nvim' },
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-telescope/telescope-fzf-native.nvim' },
-    }
-  }
-  use { "nvim-telescope/telescope-ui-select.nvim" }
-  use { "nvim-telescope/telescope-rg.nvim" }
+	-- Telescope
+	use({ "nvim-telescope/telescope-media-files.nvim" })
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({
+		"nvim-telescope/telescope.nvim",
+		config = "require('talama.plugins.telescope')",
+		requires = {
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim" },
+		},
+	})
+	use({ "nvim-telescope/telescope-ui-select.nvim" })
 
-  --- Nvim-tree
-  use { 'kyazdani42/nvim-tree.lua', config = "require('talama.plugins.tree')" }
+	--- Nvim-tree
+	use({ "kyazdani42/nvim-tree.lua", config = "require('talama.plugins.tree')" })
 
-  -- LSP Base
-  use { 'williamboman/nvim-lsp-installer' }
-  use { 'neovim/nvim-lspconfig' }
+	-- LSP Base
+	use({ "williamboman/nvim-lsp-installer" })
+	use({ "neovim/nvim-lspconfig" })
 
-  -- LSP Cmp
-  use { 'hrsh7th/nvim-cmp', event = 'InsertEnter', config = "require('talama.plugins.cmp')" }
-  use { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' }
-  use { 'hrsh7th/cmp-nvim-lsp', after = 'cmp-nvim-lua' }
-  use { 'hrsh7th/cmp-buffer', after = 'cmp-nvim-lsp' }
-  use { 'hrsh7th/cmp-path', after = 'cmp-buffer' }
-  use { 'hrsh7th/cmp-cmdline', after = 'cmp-path' }
+	-- LSP Cmp
+	use({ "hrsh7th/nvim-cmp", event = "InsertEnter", config = "require('talama.plugins.cmp')" })
+	use({ "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" })
+	use({ "hrsh7th/cmp-nvim-lsp", after = "cmp-nvim-lua" })
+	use({ "hrsh7th/cmp-buffer", after = "cmp-nvim-lsp" })
+	use({ "hrsh7th/cmp-path", after = "cmp-buffer" })
+	use({ "hrsh7th/cmp-cmdline", after = "cmp-path" })
 
-  -- LSP Addons
-  use { 'akinsho/nvim-toggleterm.lua', branch = 'main', config = "require('talama.plugins.toggleterm')" }
-  use { 'jose-elias-alvarez/null-ls.nvim', config = "require('talama.lsp.null-ls')" }
-  use { 'RRethy/vim-illuminate', config = "require('talama.plugins.illuminate')" }
-  use { 'onsails/lspkind-nvim' } -- adds vscode-like pictograms to neovim built-in lsp
-  use { 'folke/lsp-trouble.nvim', config = "require('talama.plugins.trouble')" }
-  use { 'nvim-lua/popup.nvim' }
+	-- LSP Addons
+	use({ "akinsho/nvim-toggleterm.lua", branch = "main", config = "require('talama.plugins.toggleterm')" })
+	use({ "jose-elias-alvarez/null-ls.nvim", config = "require('talama.lsp.null-ls')" })
+	use({ "RRethy/vim-illuminate", config = "require('talama.plugins.illuminate')" })
+	use({ "onsails/lspkind-nvim" }) -- adds vscode-like pictograms to neovim built-in lsp
+	use({ "folke/lsp-trouble.nvim", config = "require('talama.plugins.trouble')" })
+	use({ "nvim-lua/popup.nvim" })
 
-  -- General
-  use { 'numToStr/Comment.nvim', config = "require('talama.plugins.comment')" }
-  use { 'JoosepAlviste/nvim-ts-context-commentstring' }
-  use { 'romgrk/barbar.nvim', requires = { 'kyazdani42/nvim-web-devicons' }, config = "require('talama.plugins.barbar')" }
-  use { 'rcarriga/nvim-notify' }
-  use { "nvim-lualine/lualine.nvim", config = "require('talama.plugins.lualine')" }
+	-- General
+	use({ "numToStr/Comment.nvim", config = "require('talama.plugins.comment')" })
+	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
+	use({
+		"romgrk/barbar.nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+		config = "require('talama.plugins.barbar')",
+	})
+	use({ "rcarriga/nvim-notify" })
+	use({ "nvim-lualine/lualine.nvim", config = "require('talama.plugins.lualine')" })
 
-  -- Language and syntax
-  use { 'windwp/nvim-autopairs', after = { 'nvim-treesitter', 'nvim-cmp' },
-    config = "require('talama.plugins.autopairs')" }
-  use { 'p00f/nvim-ts-rainbow', after = { 'nvim-treesitter' } }
-  use { 'lukas-reineke/indent-blankline.nvim', config = "require('talama.plugins.indent')" }
-  use { 'NvChad/nvim-colorizer.lua', config = "require('talama.plugins.colorizer')" }
+	-- Language and syntax
+	use({
+		"windwp/nvim-autopairs",
+		after = { "nvim-treesitter", "nvim-cmp" },
+		config = "require('talama.plugins.autopairs')",
+	})
+	use({ "p00f/nvim-ts-rainbow", after = { "nvim-treesitter" } })
+	use({ "lukas-reineke/indent-blankline.nvim", config = "require('talama.plugins.indent')" })
+	use({ "NvChad/nvim-colorizer.lua", config = "require('talama.plugins.colorizer')" })
 
-  -- Snippets
-  use { 'saadparwaiz1/cmp_luasnip' }
-  use { "rafamadriz/friendly-snippets" }
-  use { 'L3MON4D3/LuaSnip', requires = { 'rafamadriz/friendly-snippets' }, after = 'cmp_luasnip' }
+	-- Snippets
+	use({ "saadparwaiz1/cmp_luasnip" })
+	use({ "rafamadriz/friendly-snippets" })
+	use({ "L3MON4D3/LuaSnip", requires = { "rafamadriz/friendly-snippets" }, after = "cmp_luasnip" })
 
-  -- Git
-  use { 'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = "require('talama.plugins.git.signs')",
-    event = "BufRead"
-  }
-  use { 'sindrets/diffview.nvim', config = "require('talama.plugins.git.diffview')" }
-  use { 'akinsho/git-conflict.nvim', config = "require('talama.plugins.git.conflict')" }
-  use { 'kdheepak/lazygit.nvim' }
+	-- Git
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = "require('talama.plugins.git.signs')",
+		event = "BufRead",
+	})
+	use({ "sindrets/diffview.nvim", config = "require('talama.plugins.git.diffview')" })
+	use({ "akinsho/git-conflict.nvim", config = "require('talama.plugins.git.conflict')" })
+	use({ "kdheepak/lazygit.nvim" })
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
--- nvim/.config/nvim/lua/talama/plugins/telescope.lua
+++ nvim/.config/nvim/lua/talama/plugins/telescope.lua
@@ -1,129 +1,119 @@
-local status_ok, telescope = pcall(require, "telescope")
-if not status_ok then
-  return
+local telescope_ok, telescope = pcall(require, "telescope")
+if not telescope_ok then
+	return
 end
 
-telescope.load_extension('media_files')
-telescope.load_extension('fzf')
-telescope.load_extension("ui-select")
-telescope.load_extension("live_grep_args")
-
-local actions = require('telescope.actions')
+local actions_ok, actions = pcall(require, "telescope.actions")
+if not actions_ok then
+	return
+end
 
 local git_icons = {
-  added = "  ",
-  changed = " 柳",
-  copied = ">",
-  deleted = "  ",
-  renamed = "➡",
-  unmerged = "‡",
-  untracked = "?",
+	added = "  ",
+	changed = " 柳",
+	copied = ">",
+	deleted = "  ",
+	renamed = "➡",
+	unmerged = "‡",
+	untracked = "?",
 }
 
 telescope.setup({
-  defaults = {
-    file_ignore_patterns = { "node_modules" },
-    path_display         = { "smart" },
-    vimgrep_arguments    = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case'
-    },
-    layout_config        = {
-      horizontal = {
-        preview_cutoff = 120,
-      },
-      prompt_position = "top",
-    },
-    file_sorter          = require('telescope.sorters').get_fzy_sorter,
-    prompt_prefix        = ' 🔍 ',
-    color_devicons       = true,
+	defaults = {
+		-- Default configuration for telescope goes here:
+		-- config_key = value,
+		prompt_prefix = "🔍 ",
+		selection_caret = "❯ ",
+		entry_prefix = "  ",
+		git_icons = git_icons,
+		color_devicons = true,
 
-    git_icons = git_icons,
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
 
-    sorting_strategy = "ascending",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
+			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
+		},
 
-    file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
-    grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
-    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+		file_ignore_patterns = {
+			".git/",
+			"node_modules",
+		},
 
-    mappings = {
-      i = {
-        ["<C-n>"] = actions.move_selection_next,
-        ["<C-p>"] = actions.move_selection_previous,
-        ["<C-c>"] = actions.close,
-        ["<C-j>"] = actions.cycle_history_next,
-        ["<C-k>"] = actions.cycle_history_prev,
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-        ["<CR>"] = actions.select_default,
-      },
-      n = {
-        ["<C-n>"] = actions.move_selection_next,
-        ["<C-p>"] = actions.move_selection_previous,
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-      },
-    },
-  },
-  pickers = {
-    find_files = {
-      theme = "dropdown",
-      previewer = true,
-      -- find_command = { "find", "-type", "f" },
-      -- find_command = { "fd", "-H", "-I" }, -- "-H" search hidden files, "-I" do not respect to gitignore
-    },
+		vimgrep_arguments = {
+			"rg",
+			"--ignore",
+			"--hidden",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--trim",
+		},
 
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  },
-  extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
+		mappings = {
+			i = {
+				-- map actions.which_key to <C-h> (default: <C-/>)
+				-- actions.which_key shows the mappings for your picker,
+				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
+				["<C-n>"] = actions.move_selection_next,
+				["<C-p>"] = actions.move_selection_previous,
+				["<C-c>"] = actions.close,
+				["<C-j>"] = actions.cycle_history_next,
+				["<C-k>"] = actions.cycle_history_prev,
+				["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+				["<CR>"] = actions.select_default,
+				["<C-h>"] = "which_key",
+			},
+			n = {
+				["<C-n>"] = actions.move_selection_next,
+				["<C-p>"] = actions.move_selection_previous,
+				["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+			},
+		},
+	},
+	pickers = {
+		-- Default configuration for builtin pickers goes here:
+		-- picker_name = {
+		--   picker_config_key = value,
+		--   ...
+		-- }
+		-- Now the picker_config_key will be applied every time you call this
+		-- builtin picker
+	},
+	extensions = {
+		-- Your extension configuration goes here:
+		-- extension_name = {
+		--   extension_config_key = value,
+		-- }
+		-- please take a look at the readme of the extension you want to configure
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		},
 
-    -- fzf syntax
-    -- Token	Match type	Description
-    -- sbtrkt	fuzzy-match	Items that match sbtrkt
-    -- 'wild'	exact-match (quoted)	Items that include wild
-    -- ^music	prefix-exact-match	Items that start with music
-    -- .mp3$	suffix-exact-match	Items that end with .mp3
-    -- !fire	inverse-exact-match	Items that do not include fire
-    -- !^music	inverse-prefix-exact-match	Items that do not start with music
-    -- !.mp3$	inverse-suffix-exact-match	Items that do not end with .mp3
-    fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-      -- the default case_mode is "smart_case"
-    },
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown({
-        -- even more opts
-      }),
-    },
-    live_grep_args = {
-      auto_quoting = false, -- enable/disable auto-quoting
-    },
-    media_files = {
-      -- filetypes whitelist
-      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-      filetypes = { "png", "webp", "jpg", "jpeg" },
-      find_cmd = "rg", -- find command (defaults to `fd`)
-    },
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  },
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({
+				-- even more opts
+			}),
+		},
+	},
 })
+
+require("telescope").load_extension("fzf")
+require("telescope").load_extension("ui-select")
