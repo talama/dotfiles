d8fe01e36c953d8ca9677a01536d67d0bc0264a5
talama <talama@protonmail.com>
2022-07-20 18:04:13 +0200

    Simplify

--- nvim/.config/nvim/lua/talama/colorscheme.lua
+++ nvim/.config/nvim/lua/talama/colorscheme.lua
@@ -1,6 +1,6 @@
-local colorscheme = 'onedark'
+local colorscheme = "onedark"
 
-local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
-if not status_ok then
-  return
+local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not ok then
+	return
 end
