ca99cc41a8b51c38ea30d223e8981c4f039e18a0
talama <talama@protonmail.com>
2022-07-20 17:07:53 +0200

    Simplify

--- nvim/.config/nvim/lua/talama/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/talama/plugins/autopairs.lua
@@ -1,20 +1,22 @@
-local status_ok, autopairs = pcall(require, 'nvim-autopairs')
-if not status_ok then 
-  return
+local ok, autopairs = pcall(require, "nvim-autopairs")
+if not ok then
+	return
 end
 
 autopairs.setup({
-  check_ts = true, -- Treesitter integration
-  ts_config = {
-    lua = { 'string' },
-    javascript = { 'template_string' },
-    java = false,
-  }
+	check_ts = true, -- Treesitter integration
+	ts_config = {
+		lua = { "string", "source" },
+		javascript = { "string", "template_string" },
+		java = false,
+	},
+	disabled_filetype = { "TelescopePrompt", "vim" },
+	fast_wrap = {},
 })
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp_status_ok, cmp = pcall(require, 'cmp')
-if not cmp_status_ok then 
-  return
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp_ok, cmp = pcall(require, "cmp")
+if not cmp_ok then
+	return
 end
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
