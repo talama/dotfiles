c609584da7f9f03be496976bac9031403438a54d
talama <talama@protonmail.com>
2022-07-21 15:54:12 +0200

    Updated options

--- nvim/.config/nvim/lua/talama/options.lua
+++ nvim/.config/nvim/lua/talama/options.lua
@@ -1,69 +1,71 @@
 local options = {
-  clipboard      = "unnamed,unnamedplus", --- Copy-paste between vim and everything else
-  cmdheight      = 2, --- Give more space for displaying messages
-  completeopt    = { "menuone", "noselect" }, --- Better autocompletion
-  cursorline     = true, --- Highlight of current line
-  emoji          = false, --- Fix emoji display
-  expandtab      = true, --- Use spaces instead of tabs
-  foldlevelstart = 99, --- Expand all folds by default
-  foldtext       = "CustomFold()", --- Emit custom function for foldtext
-  ignorecase     = true, --- Needed for smartcase
-  laststatus     = 3, --- Have a global statusline at the bottom instead of one for each window
-  lazyredraw     = true, --- Makes macros faster & prevent errors in complicated mappings
-  mouse          = "a", --- Enable mouse
-  number         = true, --- Shows current line number
-  pumheight      = 10, --- Max num of items in completion menu
-  relativenumber = true, --- Enables relative number
-  scrolloff      = 8, --- Always keep space when scrolling to bottom/top edge
-  sidescrolloff  = 8, --- Always keep space when scrolling to the side
-  shiftwidth     = 2, --- Change a number of space characeters inseted for indentation
-  showtabline    = 2, --- Always show tabs
-  signcolumn     = "yes:2", --- Add extra sign column next to line number
-  smartcase      = true, --- Uses case in search
-  smartindent    = true, --- Makes indenting smart
-  smarttab       = true, --- Makes tabbing smarter will realize you have 2 vs 4
-  softtabstop    = 2, --- Insert 2 spaces for a tab
-  splitright     = true, --- Vertical splits will automatically be to the right
-  swapfile       = false, --- Swap not needed
-  tabstop        = 2, --- Insert 2 spaces for a tab
-  termguicolors  = true, --- Correct terminal colors
-  timeoutlen     = 200, --- Faster completion (cannot be lower than 200 because then commenting doesn't work)
-  undofile       = true, --- Sets undo to file
-  updatetime     = 100, --- Faster completion
-  viminfo        = "'1000", --- Increase the size of file history
-  wildignore     = "*node_modules/**", --- Don't search inside Node.js modules (works for gutentag)
-  wrap           = false, --- Display long lines as just one line
-  writebackup    = false, --- Not needed
-  -- Neovim defaults
-  autoindent     = true, --- Good auto indent
-  backspace      = "indent,eol,start", --- Making sure backspace works
-  backup         = false, --- Recommended by coc
-  conceallevel   = 0, --- Show `` in markdown files
-  encoding       = "utf-8", --- The encoding displayed
-  errorbells     = false, --- Disables sound effect for errors
-  fileencoding   = "utf-8", --- The encoding written to file
-  incsearch      = true, --- Start searching before pressing enter
-  showmode       = false, --- Don't show things like -- INSERT -- anymore
+	autoindent = true, --- Good auto indent
+	backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
+	backup = false, --- no backup file
+	clipboard = "unnamed,unnamedplus", --- Copy-paste between vim and everything else
+	cmdheight = 2, --- Give more space for displaying messages
+	completeopt = { "menuone", "noselect" }, --- Better autocompletion
+	conceallevel = 0, --- Show `` in markdown files
+	cursorline = true, --- Highlight of current line
+	emoji = false, --- Fix emoji display
+	encoding = "utf-8", --- The encoding displayed
+	errorbells = false, --- Disables sound effect for errors
+	expandtab = true, --- Use spaces instead of tabs
+	fileencoding = "utf-8", --- The encoding written to file
+	fillchars = { eob = " " }, -- Disable `~` on nonexistent lines
+	foldlevelstart = 99, --- Expand all folds by default
+	foldtext = "CustomFold()", --- Emit custom function for foldtext
+	history = 100, -- Number of commands to remember in a history table
+	ignorecase = true, --- Needed for smartcase
+	incsearch = true, --- Start searching before pressing enter
+	laststatus = 3, --- Have a global statusline at the bottom instead of one for each window
+	lazyredraw = true, --- Makes macros faster & prevent errors in complicated mappings
+	mouse = "a", --- Enable mouse
+	number = true, --- Shows current line number
+	pumheight = 10, --- Max num of items in completion menu
+	relativenumber = true, --- Enables relative number
+	scrolloff = 8, --- Always keep space when scrolling to bottom/top edge
+	shiftwidth = 2, --- Change a number of space characeters inseted for indentation
+	showmode = false, --- Don't show things like -- INSERT -- anymore
+	showtabline = 2, --- Always show tabs
+	sidescrolloff = 8, --- Always keep space when scrolling to the side
+	signcolumn = "yes:2", --- Add extra sign column next to line number
+	smartcase = true, --- Uses case in search
+	smartindent = true, --- Makes indenting smart
+	smarttab = true, --- Makes tabbing smarter will realize you have 2 vs 4
+	softtabstop = 2, --- Insert 2 spaces for a tab
+	splitbelow = true, -- Splitting a new window below the current one
+	splitright = true, --- Vertical splits will automatically be to the right
+	swapfile = false, --- Swap not needed
+	tabstop = 2, --- Insert 2 spaces for a tab
+	termguicolors = true, --- Correct terminal colors
+	timeoutlen = 200, --- Faster completion (cannot be lower than 200 because then commenting doesn't work)
+	undofile = true, --- Sets undo to file
+	updatetime = 100, --- Faster completion
+	wildignore = "*node_modules/**", --- Don't search inside Node.js modules
+	wrap = false, --- Display long lines as just one line
+	writebackup = false, --- Not needed
 }
 
 local globals = {
-  fillchars               = "fold:\\ ", --- Fill chars needed for folds
-  mapleader               = ' ', --- Map leader key to SPC
-  speeddating_no_mappings = 1, --- Disable default mappings for speeddating
+	fillchars = "fold:\\ ", --- Fill chars needed for folds
+	highlighturl_enabled = true, -- highlight URLs by default
+	mapleader = " ", --- Map leader key to SPC
+	speeddating_no_mappings = 1, --- Disable default mappings for speeddating
 }
 
-vim.opt.shortmess:append('c');
-vim.opt.formatoptions:remove('c');
-vim.opt.formatoptions:remove('r');
-vim.opt.formatoptions:remove('o');
-vim.notify = require('notify')
+vim.opt.shortmess:append("c")
+vim.opt.formatoptions:remove("c")
+vim.opt.formatoptions:remove("r")
+vim.opt.formatoptions:remove("o")
+vim.notify = require("notify")
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 for k, v in pairs(globals) do
-  vim.g[k] = v
+	vim.g[k] = v
 end
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd("set whichwrap+=<,>,[,],h,l")
