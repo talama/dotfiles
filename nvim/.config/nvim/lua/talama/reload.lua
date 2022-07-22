0b01628c3b340e2fe63dc9c1c9e1d5f058825d74
talama <talama@protonmail.com>
2022-07-22 16:03:46 +0200

    New Implementation

--- nvim/.config/nvim/lua/talama/reload.lua
+++ nvim/.config/nvim/lua/talama/reload.lua
@@ -1,10 +1,10 @@
 function _G.ReloadConfig()
-  for name, _ in pairs(package.loaded) do
-    if name:match('^Talama') then
-      package.loaded[name] = nil
-    end
-  end
+	for name, _ in pairs(package.loaded) do
+		if string.match(name, "^Talama") then
+			package.loaded[name] = nil
+		end
+	end
 
-  dofile(vim.env.MYVIMRC)
-  vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
+	dofile(vim.env.MYVIMRC)
+	vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
 end
