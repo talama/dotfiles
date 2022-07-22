e637097d5dad8420e7077715a9606f32b9b9b8cd
talama <talama@protonmail.com>
2022-07-22 18:07:10 +0200

    Added autocommands

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,7 @@ pcall(require, "impatient")
 
 require("talama.options")
 require("talama.keymaps")
+require("talama.autocommands")
 require("talama.reload")
 require("talama.plugins")
 require("talama.colorscheme")
