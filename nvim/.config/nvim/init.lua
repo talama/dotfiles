322424b2475cdf1b13edec3e66516045e758a152
talama <talama@protonmail.com>
2022-07-22 17:44:23 +0200

    Simplify

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,8 @@
-pcall(require, 'impatient')
+pcall(require, "impatient")
 
-require 'talama.options'
-require 'talama.keymaps'
-require 'talama.reload'
-require 'talama.plugins'
-require 'talama.colorscheme'
-require 'talama.lsp.setup'
+require("talama.options")
+require("talama.keymaps")
+require("talama.reload")
+require("talama.plugins")
+require("talama.colorscheme")
+require("talama.lsp.setup")
