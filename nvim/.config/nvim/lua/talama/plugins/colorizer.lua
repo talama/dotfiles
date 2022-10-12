c6dbaa860939a4a6d8fc2d9b270d9b5b8d6ecff9
talama <talama@protonmail.com>
2022-10-12 15:26:54 +0200

    Fixed filetypes error

--- nvim/.config/nvim/lua/talama/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/talama/plugins/colorizer.lua
@@ -3,16 +3,22 @@ if not ok then
 	return
 end
 
-colorizer.setup({ "*" }, {
-	RGB = true, -- #RGB hex codes
-	RRGGBB = true, -- #RRGGBB hex codes
-	names = true, -- "Name" codes like Blue or blue
-	RRGGBBAA = true, -- #RRGGBBAA hex codes
-	rgb_fn = true, -- CSS rgb() and rgba() functions
-	hsl_fn = true, -- CSS hsl() and hsla() functions
-	css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-	css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-
-	-- Available modes: foreground, background, virtualtext
-	mode = "background",
+colorizer.setup({
+	filetypes = { "*" },
+	user_default_options = {
+		RGB = true, -- #RGB hex codes
+		RRGGBB = true, -- #RRGGBB hex codes
+		names = true, -- "Name" codes like Blue or blue
+		RRGGBBAA = false, -- #RRGGBBAA hex codes
+		AARRGGBB = false, -- 0xAARRGGBB hex codes
+		rgb_fn = false, -- CSS rgb() and rgba() functions
+		hsl_fn = false, -- CSS hsl() and hsla() functions
+		css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+		css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+		-- Available modes for `mode`: foreground, background,  virtualtext
+		mode = "background", -- Set the display mode.
+		virtualtext = "■",
+	},
+	-- all the sub-options of filetypes apply to buftypes
+	buftypes = {},
 })
