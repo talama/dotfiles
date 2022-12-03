973e329a239852e9b1ddabf6045d78f70a001d68
talama <talama@protonmail.com>
2022-12-03 23:30:22 +0100

    Updated colorscheme config

--- nvim/.config/nvim/lua/talama/colorscheme.lua
+++ nvim/.config/nvim/lua/talama/colorscheme.lua
@@ -1,14 +1,25 @@
-local colorscheme = 'onedarkpro'
+local colorscheme = "onedarkpro"
 
-if colorscheme == 'onedark' then
-  require('onedark').setup{
-  style = 'dark'
-  }
-  require('onedark').load()  
-elseif colorscheme == 'onedarkpro' then
-  require('onedarkpro').setup({
-    theme = "onedark_vivid"
-  })
-  vim.cmd('colorscheme onedarkpro')
-else vim.cmd('colorscheme darkblue')
+if colorscheme == "onedark" then
+	require("onedark").setup({
+		style = "dark",
+	})
+	require("onedark").load()
+elseif colorscheme == "onedarkpro" then
+	require("onedarkpro").setup({
+		theme = "onedark_vivid",
+		highlights = {
+			["@variable.javascript"] = { fg = "${yellow}" },
+		},
+		styles = {
+			comments = "italic",
+		},
+		options = {
+			cursorline = true,
+			transparency = true,
+		},
+	})
+	vim.cmd("colorscheme onedarkpro")
+else
+	vim.cmd("colorscheme darkblue")
 end
