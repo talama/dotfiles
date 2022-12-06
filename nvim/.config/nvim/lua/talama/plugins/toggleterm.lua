6814288bdf8579c4d4017f09a92ee4f917f454ef
talama <talama@protonmail.com>
2022-12-06 15:54:51 +0100

    Reduced terminal height

--- nvim/.config/nvim/lua/talama/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/talama/plugins/toggleterm.lua
@@ -7,7 +7,7 @@ toggleterm.setup({
 	-- size can be a number or function which is passed the current terminal
 	size = function(term)
 		if term.direction == "horizontal" then
-			return 15
+			return 10
 		elseif term.direction == "vertical" then
 			return vim.o.columns * 0.4
 		end
