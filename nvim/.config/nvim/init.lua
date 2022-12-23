72a103601a6a75b7e543f37f20c6a541faa9fde5
talama <talama@protonmail.com>
2022-12-24 00:38:10 +0100

    Added packer and impatient

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
 if vim.g.vscode then
 	print("vscode")
 else
-	-- require("talama.impatient")
+	require("talama.impatient")
 	-- require("talama.options")
 	-- require("talama.keymaps")
 	require("talama.plugins")
