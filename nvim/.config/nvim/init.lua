0b05895244b278dec0df5c60b42dbe659876f358
talama <talama@protonmail.com>
2022-12-24 00:24:13 +0100

    New Config Start

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,10 @@
 if vim.g.vscode then
 	print("vscode")
 else
-	print("nvim")
-	require("talama.impatient")
-	require("talama.options")
-	require("talama.keymaps")
+	-- require("talama.impatient")
+	-- require("talama.options")
+	-- require("talama.keymaps")
 	require("talama.plugins")
-	require("talama.colorscheme")
-	require("talama.plugins.lsp")
+	-- require("talama.colorscheme")
+	-- require("talama.plugins.lsp")
 end
--- nvim/.config/nvim/lua/talama/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-local colorscheme = "onedarkpro"
-
-if colorscheme == "onedarkpro" then
-	require("talama.plugins.colorschemes.onedarkpro")
-	vim.cmd("colorscheme onedark")
-elseif colorscheme == "catpuccin" then
-	require("talama.plugins.colorschemes.catpuccin")
-end
