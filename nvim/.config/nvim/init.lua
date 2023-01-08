b002f0ede2bc99b64b7a547cca1ff2798a57485d
talama <talama@protonmail.com>
2023-01-08 20:17:55 +0100

    Added basic config for vscode plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,11 @@
 if vim.g.vscode then
 	print("vscode")
+	require("talama.vscode.settings")
 else
 	require("talama.impatient")
 	require("talama.options")
 	require("talama.keymaps")
 	require("talama.plugins")
-  require("talama.colorscheme")
+	require("talama.colorscheme")
 	require("talama.plugins.lsp")
 end
---
+++ nvim/.config/nvim/lua/talama/vscode/settings.lua
@@ -0,0 +1,26 @@
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true, noremap = true }
+
+-- Function for normal mode
+local function nmap(shortcut, command)
+	keymap("n", shortcut, command, opts)
+end
+
+local function xmap(shortcut, command)
+	keymap("x", shortcut, command, opts)
+end
+
+-- Better window navigation
+nmap("<C-j>", "<cmd> call VSCodeNotify('workbench.action.navigateDown') <CR>")
+xmap("<C-j>", "<cmd> call VSCodeNotify('workbench.action.navigateDown') <CR>")
+nmap("<C-k>", "<cmd> call VSCodeNotify('workbench.action.navigateUp') <CR>")
+xmap("<C-k>", "<cmd> call VSCodeNotify('workbench.action.navigateUp') <CR>")
+nmap("<C-h>", "<cmd> call VSCodeNotify('workbench.action.navigateLeft') <CR>")
+xmap("<C-h>", "<cmd> call VSCodeNotify('workbench.action.navigateLeft') <CR>")
+nmap("<C-l>", "<cmd> call VSCodeNotify('workbench.action.navigateRight') <CR>")
+xmap("<C-l>", "<cmd> call VSCodeNotify('workbench.action.navigateRight') <CR>")
+
+-- which-key
+nmap("<Space>", "<cmd> call VSCodeNotify('whichkey.show') <cr>")
+xmap("<Space>", "<cmd> call VSCodeNotify('whichkey.show') <cr>")
