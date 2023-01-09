d8808eb337b141408ef46ceda638041706ad60c9
talama <talama@protonmail.com>
2023-01-09 09:17:11 +0100

    Updated config

--- nvim/.config/nvim/lua/talama/plugins/lualine.lua
+++ nvim/.config/nvim/lua/talama/plugins/lualine.lua
@@ -1,13 +1,13 @@
 local ok, lualine = pcall(require, "lualine")
-if not ok then 
-  return
+if not ok then
+	return
 end
 
 local diagnostic = {
-  "diagnostic",
-  sources = { "nvim-diagnostic" },
-  sections = { "error", "warn" },
-  symbols = { error = " ", warn = " " },
+	"diagnostic",
+	sources = { "nvim-diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
 	colored = true,
 	update_in_insert = false,
 	always_visible = true,
@@ -16,36 +16,36 @@ local diagnostic = {
 local diff = {
 	"diff",
 	colored = true,
-  -- changes diff symbols
+	-- changes diff symbols
 	symbols = { added = "", modified = "", removed = "" },
 }
 
 local filetype = {
-  "filetype",
-  icons_enabled = false,
+	"filetype",
+	icons_enabled = false,
 }
 
 local location = {
-  "location",
-  padding = 0,
+	"location",
+	padding = 0,
 }
 
 lualine.setup({
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch" },
-    lualine_c = { "filename", diff },
-    lualine_d = { diagnostic },
-    lualine_x = { "fileformat", "encoding", filetype },
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard", "Outline" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch" },
+		lualine_c = { "filename", diff },
+		lualine_d = { diagnostic },
+		lualine_x = { "fileformat", "encoding", filetype },
 		lualine_y = { location },
 		lualine_z = { "progress" },
-  },
+	},
 })
