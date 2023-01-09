07b9a807d2d44580cc3c57f21c12f96b75c700a3
talama <talama@protonmail.com>
2023-01-09 11:17:54 +0100

    Added lazygit toggle

--- nvim/.config/nvim/lua/talama/utils.lua
+++ nvim/.config/nvim/lua/talama/utils.lua
@@ -17,4 +17,24 @@ function M.smart_quit()
 	end
 end
 
+function M.lazygit_toggle()
+	local Terminal = require("toggleterm.terminal").Terminal
+	local lazygit = Terminal:new({
+		cmd = "lazygit",
+		hidden = true,
+		direction = "float",
+		float_opts = {
+			border = "none",
+			width = 100000,
+			height = 100000,
+		},
+		on_open = function(_)
+			vim.cmd("startinsert!")
+		end,
+		on_close = function(_) end,
+		count = 99,
+	})
+	lazygit:toggle()
+end
+
 return M
