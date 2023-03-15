588284c5a1687930b82000c947bd6e99a13af585
talama <talama@protonmail.com>
2023-03-15 18:42:20 +0100

    fix(null-ls): re-enabled lsp

--- nvim/.config/nvim/lua/talama/plugins.lua
+++ nvim/.config/nvim/lua/talama/plugins.lua
@@ -91,28 +91,28 @@ return packer.startup(function(use)
 
 	-- LSP
 	-- lsp-zero
-	-- use({
-	-- 	"VonHeikemen/lsp-zero.nvim",
-	-- 	requires = {
-	-- 		-- LSP Support
-	-- 		{ "neovim/nvim-lspconfig" },
-	-- 		{ "williamboman/mason.nvim" },
-	-- 		{ "williamboman/mason-lspconfig.nvim" },
-	--
-	-- 		-- Autocompletion
-	-- 		{ "hrsh7th/nvim-cmp" },
-	-- 		{ "hrsh7th/cmp-nvim-lsp" },
-	-- 		{ "hrsh7th/cmp-nvim-lua" },
-	-- 		{ "hrsh7th/cmp-buffer" },
-	-- 		{ "hrsh7th/cmp-path" },
-	-- 		{ "saadparwaiz1/cmp_luasnip" },
-	-- 		{ "hrsh7th/cmp-cmdline" },
-	--
-	-- 		-- Snippets
-	-- 		{ "L3MON4D3/LuaSnip" },
-	-- 		{ "rafamadriz/friendly-snippets" },
-	-- 	},
-	-- })
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-cmdline" },
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
 	--
 	-- Null-ls for linting/formatting
 	use({ "jose-elias-alvarez/null-ls.nvim", config = "require('talama.plugins.lsp.null-ls')" })
