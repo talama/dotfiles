ccbc407434fecf6f1996306fde7c639f4b1c1cae
talama <talama@protonmail.com>
2023-04-18 14:51:37 +0200

    feat(nvim-tree): migrated to on_attach

--- nvim/.config/nvim/lua/talama/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/talama/plugins/nvim-tree.lua
@@ -10,28 +10,38 @@ local git_icons = {
 	ignored = "◌",
 }
 
-local keymappings = {
-	{ key = { "l", "<CR>", "o" }, action = "edit" },
-	{ key = "h", action = "close_node" },
-	{ key = "v", action = "vsplit" },
-	{ key = "x", action = "split" },
-	{ key = "<C-t>", action = "tabnew" },
-	{ key = "<Tab>", action = "preview" },
-	{ key = "H", action = "toggle_dotfiles" },
-	{ key = "R", action = "refresh" },
-	{ key = "a", action = "create" },
-	{ key = "d", action = "remove" },
-	{ key = "D", action = "trash" },
-	{ key = "r", action = "rename" },
-	{ key = "x", action = "cut" },
-	{ key = "c", action = "copy" },
-	{ key = "p", action = "paste" },
-	{ key = "y", action = "copy_name" },
-	{ key = "Y", action = "copy_path" },
-	{ key = "gy", action = "copy_absolute_path" },
-}
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+	vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+	vim.keymap.set("n", "x", api.node.open.horizontal, opts("Open: Horizontal Split"))
+	vim.keymap.set("n", "<C-t>", api.node.open.tab, opts("Open: New Tab"))
+	vim.keymap.set("n", "<Tab>", api.node.open.preview, opts("Open Preview"))
+	vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+	vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+	vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+	vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+	vim.keymap.set("n", "D", api.fs.trash, opts("Trash"))
+	vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+	vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+	vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+	vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+	vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+	vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+	vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+end
 
 require("nvim-tree").setup({
+	on_attach = on_attach,
+
 	-- disables netrw completely
 	disable_netrw = true,
 	-- hijack netrw window on startup
@@ -146,8 +156,6 @@ require("nvim-tree").setup({
 			-- custom only false will merge the list with the default mappings
 			-- if true, it will only use your list to set the mappings
 			custom_only = true,
-			-- list of mappings to set on the tree manually
-			list = keymappings,
 		},
 		number = false,
 		relativenumber = false,
