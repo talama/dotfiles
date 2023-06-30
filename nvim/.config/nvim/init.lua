6521e6f25d6447ac74aca88bdde5d714fa3a69b5
talama <talama@protonmail.com>
2023-06-30 19:07:06 +0200

    feat: updated vscode config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,6 @@ if vim.g.vscode then
 	require("talama.vscode.settings")
 	require("talama.vscode.options")
 	require("talama.vscode.keymaps")
-	require("talama.colorscheme")
 else
 	require("talama.impatient")
 	require("talama.options")
