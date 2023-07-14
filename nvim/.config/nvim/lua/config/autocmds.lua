42ffff61928afca043a234e082f31221afac4a7f
talama <talama@proton.me>
2023-07-15 00:39:14 +0200

    feat: enabled treesitter folding

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+  group = augroup("folding"),
+  pattern = "*",
+  callback = function()
+    vim.opt.foldlevel = 99
+  end,
+})
