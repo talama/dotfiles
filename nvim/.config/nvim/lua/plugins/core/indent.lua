aef982550d66110beffe77b20f0c2c6bf9a9abe1
talama <talama@proton.me>
2023-10-05 11:33:36 +0200

    feat: updated indent-blankline to version 3

--- nvim/.config/nvim/lua/plugins/core/indent.lua
+++ nvim/.config/nvim/lua/plugins/core/indent.lua
@@ -1,13 +1,6 @@
-return  {
-  'lukas-reineke/indent-blankline.nvim',
-  opts = {
-    buftype_exclude = { "terminal", "nofile", "prompt", "quickfix"},
-	  filetype_exclude = { 'lspinfo', 'packer', 'checkhealth', 'help', 'man', 'dashboard', 'git', 'markdown', 'text', 'terminal', "Trouble", "TelescopePrompt", "Float" },
-	  show_current_context = true,
-	  show_current_context_start = false,
-	  show_end_of_line = false,
-	  show_trailing_blankline_indent = false,
-	  space_char_blankline = " ",
-	  use_treesitter = true,
-  }
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	config = function(_, opts)
+		require("ibl").setup(opts)
+	end,
 }
