1da329961cf8de794871ec983ea80b8a80b5ab47
talama <talama@proton.me>
2023-12-19 22:37:35 +0100

    feat: changed colors for strings and obj properties

--- nvim/.config/nvim/lua/plugins/core/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/core/catppuccin.lua
@@ -28,11 +28,13 @@ return {
 		custom_highlights = function(colors)
 			return {
 				["@variable"] = { fg = colors.text, style = {} },
-				["@property"] = { fg = colors.text, style = {} },
+				["@property"] = { fg = colors.red, style = {} },
 				["@method.call"] = { fg = colors.blue },
 				["@type"] = { fg = colors.yellow },
 				["@lsp.mod.readonly"] = { fg = colors.yellow },
 				["@lsp.type.parameter"] = { fg = colors.teal },
+        ["@string"] = { fg = colors.flamingo },
+        ["@function.builtin"] = { fg = colors.blue},
 			}
 		end,
 		integrations = {
