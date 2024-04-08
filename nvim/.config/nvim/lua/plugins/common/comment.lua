05cef0bf9e862dcc7ba36e5022552113fd86ea1e
talama <talama@proton.me>
2024-04-08 10:41:36 +0200

    feat(Comment): updated config

--- nvim/.config/nvim/lua/plugins/common/comment.lua
+++ nvim/.config/nvim/lua/plugins/common/comment.lua
@@ -52,26 +52,7 @@ return {
     -- @type function|nil
     -- @param ctx Ctx
     pre_hook = function(ctx)
-      -- Only calculate commentstring for tsx filetypes
-      if vim.bo.filetype == "typescriptreact" then
-        local U = require("Comment.utils")
-
-        -- Determine whether to use linewise or blockwise commentstring
-        local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-        -- Determine the location where to calculate commentstring from
-        local location = nil
-        if ctx.ctype == U.ctype.blockwise then
-          location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
-
-        return require("ts_context_commentstring.internal").calculate_commentstring({
-          key = type,
-          location = location,
-        })
-      end
+      require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
     end,
 
     -- Post-hook, called after commenting is done
