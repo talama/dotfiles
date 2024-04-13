ba1cdc47519fc09346cd6d1792e9ffabdfa3f2bb
talama <talama@proton.me>
2024-04-13 07:13:38 +0200

    fix(nvim-dap): fixed js-debug-adapter path

--- nvim/.config/nvim/lua/plugins/lang/typescript.lua
+++ nvim/.config/nvim/lua/plugins/lang/typescript.lua
@@ -1,65 +1,64 @@
 return {
-	-- Treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			if type(opts.ensure_installed) == "table" then
-				vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
-			end
-		end,
-	},
-	{ "jose-elias-alvarez/typescript.nvim" },
-	{
-		"mfussenegger/nvim-dap",
-		optional = true,
-		dependencies = {
-			{
-				"williamboman/mason.nvim",
-				opts = function(_, opts)
-					opts.ensure_installed = opts.ensure_installed or {}
-					table.insert(opts.ensure_installed, "js-debug-adapter")
-				end,
-			},
-		},
-		opts = function()
-			local dap = require("dap")
-			if not dap.adapters["pwa-node"] then
-				require("dap").adapters["pwa-node"] = {
-					type = "server",
-					host = "localhost",
-					port = "${port}",
-					executable = {
-						command = "node",
-						-- 💀 Make sure to update this path to point to your installation
-						args = {
-							require("mason-registry").get_package("js-debug-adapter"):get_install_path()
-								.. "/js-debug/src/dapDebugServer.js",
-							"${port}",
-						},
-					},
-				}
-			end
-			for _, language in ipairs({ "typescript", "javascript" }) do
-				if not dap.configurations[language] then
-					dap.configurations[language] = {
-						{
-							type = "pwa-node",
-							request = "launch",
-							name = "Launch file",
-							program = "${file}",
-							cwd = "${workspaceFolder}",
-						},
-						{
-							type = "pwa-node",
-							request = "attach",
-							name = "Attach",
-							processId = require("dap.utils").pick_process,
-							cwd = "${workspaceFolder}",
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+      end
+    end,
+  },
+  { "jose-elias-alvarez/typescript.nvim" },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          table.insert(opts.ensure_installed, "js-debug-adapter")
+        end,
+      },
+    },
+    opts = function()
+      local dap = require("dap")
+      if not dap.adapters["pwa-node"] then
+        require("dap").adapters["pwa-node"] = {
+          type = "server",
+          host = "localhost",
+          port = "${port}",
+          executable = {
+            command = "node",
+            -- 💀 Make sure to update this path to point to your installation
+            args = {
+              os.getenv('HOME') .. '/.local/share/nvim/mason/packages/js-debug-adapter',
+              "${port}",
+            },
+          },
+        }
+      end
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        if not dap.configurations[language] then
+          dap.configurations[language] = {
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",
+              cwd = "${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "attach",
+              name = "Attach",
+              processId = require("dap.utils").pick_process,
+              cwd = "${workspaceFolder}",
               sourceMaps = "true",
-						},
-					}
-				end
-			end
-		end,
-	},
+            },
+          }
+        end
+      end
+    end,
+  },
 }
