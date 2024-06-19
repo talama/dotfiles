7a89819f74d884f6639e050e78a6cd5c1402428f
talama <talama@proton.me>
2024-06-19 17:01:45 +0200

    fix: updated for neo-tree

--- nvim/.config/nvim/lua/plugins/extra/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/extra/bufferline.lua
@@ -17,7 +17,7 @@ return {
         -- stylua: ignore
         right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
         diagnostics = "nvim_lsp",
-        offsets = { { filetype = "NvimTree", text = "NvimTree", padding = 1 } },
+        offsets = { { filetype = "neo-tree", text = "Neo-Tree", padding = 1 } },
         separator_style = "slant",
       },
     })
