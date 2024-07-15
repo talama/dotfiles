15394e48e43293b4ec5e4160775f8372a8772ef6
talama <talama@proton.me>
2024-07-15 15:27:29 +0200

    updated keybinding for v3

--- nvim/.config/nvim/lua/plugins/extra/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/extra/whichkey.lua
@@ -1,29 +1,26 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    defaults = {
-      mode = { "n", "v" },
-      ["g"] = { name = "+goto" },
-      ["]"] = { name = "+next" },
-      ["["] = { name = "+prev" },
-      ["<leader>b"] = { name = "+buffer" },
-      ["<leader>c"] = { name = "+code" },
-      ["<leader>f"] = { name = "+file/find" },
-      ["<leader>q"] = { name = "+quit/session" },
-      ["<leader>s"] = { name = "+search" },
-      ["<leader>u"] = { name = "+ui" },
-      ["<leader>w"] = { name = "+windows" },
-      ["<leader>x"] = { name = "+diagnostics/quickfix" },
-    },
-  },
-  config = function(_, opts)
-    local wk = require("which-key")
-    wk.setup(opts)
-    wk.register(opts.defaults)
-  end,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			mode = { "n", "v" },
+			{ "g", group = "+goto" },
+			{ "]", group = "+next" },
+			{ "[", group = "+prev" },
+			{ "<leader>b", group = "+buffer" },
+			{ "<leader>c", group = "+code" },
+			{ "<leader>d", group = "+debug" },
+			{ "<leader>f", group = "+file/find" },
+			{ "<leader>q", group = "+quit.session" },
+			{ "<leader>s", group = "+search" },
+			{ "<leader>u", group = "+ui" },
+			{ "<leader>w", group = "+windows" },
+			{ "<leader>x", group = "+diagnostics/quickfix" },
+		})
+	end,
 }
