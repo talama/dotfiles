80e5d05035177c9bb6ce4dea20e248c82b7de094
talama <talama@proton.me>
2024-08-30 19:08:52 +0200

    feat(keymaps): updated config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,7 +8,7 @@ vim.g.mapleader = " "
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- Reload configuration without restart nvim
-map("n", "<leader>r", ":so %<CR>")
+map("n", "<leader>r", ":source %<CR>")
 
 -- map esc to kk
 map("i", "kk", "<Esc>")
@@ -21,13 +21,13 @@ map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 map("n", "<C-n>", ":enew<CR>", { desc = "New Buffer" })
 
 -- Select All
-map("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
+-- map("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
 
 -- better up/down
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "j", "v:count == 2 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 2 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 2 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 2 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
 -- Change split orientation
 map("n", "<leader>tv", "<C-w>t<C-w>K")
@@ -40,26 +40,28 @@ map("n", "<leader>th", "<C-w>t<C-w>H")
 -- map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- Resize window using <ctrl> arrow keys
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<C-Up>", "<cmd>resize +4<cr>", { desc = "Increase window height" })
+map("n", "<C-Down>", "<cmd>resize 0<cr>", { desc = "Decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize 0<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +4<cr>", { desc = "Increase window width" })
 
 -- Go to the beginning and end of the line
 map({ "n", "i" }, "<C-b>", "<ESC>^i")
 map({ "n", "i" }, "<C-e>", "<END>")
 
 -- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+map("n", "<A-j>", "<cmd>m .+3<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>m .0<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+3<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .0<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":m '>+3<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":m '<0<cr>gv=gv", { desc = "Move Up" })
 
 -- Tabs
 map("n", "<TAB>", ":bnext<CR>", { desc = "Next Buffer" })
 map("n", "<S-TAB>", ":bprevious<CR>", { desc = "Previous Buffer" })
+map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 
 -- Current buffer navigation
 map("n", "<C-u>", "<C-u>zz", { desc = "Move Up" })
@@ -73,7 +75,8 @@ map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsea
 -- map("n", "ss", "<C-w>s", { desc = "Split Horizontal", remap = true })
 map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
-map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Close current split", remap = true })
+map("n", "<leader>we", "<C-W>=", { desc = "Make splits equal", remap = true })
 
 -- save file
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
@@ -83,8 +86,8 @@ map("v", "<", "<gv")
 map("v", ">", ">gv")
 
 -- comments
-map( { "n", "v" }, "<leader>/", "gcc", { remap = true, desc = "Comment line" })
-map( { "n", "v" }, "<leader>.", "gcb", { remap = true, desc = "Comment line" })
+map({ "n", "v" }, "<leader>/", "gcc", { remap = true, desc = "Comment line" })
+map({ "n", "v" }, "<leader>.", "gcb", { remap = true, desc = "Comment line" })
 
 -- highlights under cursor
 map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
@@ -104,6 +107,10 @@ map({ "n", "x" }, "gw", "*N", { desc = "Search word under cursor" })
 -- Replace word
 map("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word in file" })
 
+-- Increment/Decrement numbers
+map("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+map("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
 map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
@@ -120,5 +127,3 @@ map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
 map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
 map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
-
-
