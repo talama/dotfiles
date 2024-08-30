f566615c632f1d163f0cb8c44999c0f7b560bce6
talama <talama@proton.me>
2024-08-30 19:41:11 +0200

    feat(trouble): first config

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+	opts = {
+		focus = true,
+	},
+	cmd = "Trouble",
+	keys = {
+		{ "<leader>xw", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>xb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+		{ "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+		{
+			"<leader>xS",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP references/definitions/... (Trouble)",
+		},
+		{ "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+		{ "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+	},
+}
