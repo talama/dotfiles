8f630c508f27a624de86b0884535d35cf73e58f0
talama <talama@proton.me>
2024-08-31 16:21:36 +0200

    feat(nvim-surround): initial config

---
+++ nvim/.config/nvim/lua/plugins/shared/surround.lua
@@ -0,0 +1,12 @@
+-- The three "core" operations of add/delete/change can be done with the keymaps ys{motion}{char}, ds{char}, and cs{target}{replacement}, respectively
+-- ysst to surround with an html tag
+return {
+	"kylechui/nvim-surround",
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({
+			-- Configuration here, or leave empty to use defaults
+		})
+	end,
+}
