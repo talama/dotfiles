b1ba48a935cdcaf1579f1c06e4e03b467b4cd92b
talama <talama@proton.me>
2024-08-31 16:55:47 +0200

    feat(which-key): updated keymaps

---
+++ nvim/.config/nvim/lua/plugins/ui/which-key.lua
@@ -0,0 +1,36 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			mode = { "n", "v" },
+			{ "g", group = "+goto" },
+			{ "]", group = "+next" },
+			{ "[", group = "+prev" },
+			{ "<leader>b", group = "+buffer" },
+			{ "<leader>c", group = "+code" },
+			{ "<leader>d", group = "+debug" },
+			{ "<leader>f", group = "+file/find" },
+			{ "<leader>g", group = "+git" },
+			{ "<leader>q", group = "+quit.session" },
+			{ "<leader>s", group = "+search" },
+			{ "<leader>t", group = "+tabs" },
+			{ "<leader>u", group = "+ui" },
+			{ "<leader>w", group = "+windows" },
+			{ "<leader>x", group = "+diagnostics/quickfix" },
+			{ "[", group = "prev" },
+			{ "]", group = "next" },
+			{ "g", group = "goto" },
+		})
+	end,
+}
