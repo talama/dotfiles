b9c9ff1d7b0420c019d99ca037f6735be0bdfc73
talama <talama@proton.me>
2024-09-01 17:55:48 +0200

    feat: added folding options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,7 +5,7 @@ local opt = vim.opt -- Set options (global/buffer/windows-scoped)
 -- General
 -----------------------------------------------------------
 opt.mouse = "a" -- Enable mouse support
-opt.clipboard = "unnamedplus" -- Copy/paste to system clipboard
+opt.clipboard:append("unnamedplus") -- Copy/paste to system clipboard
 opt.swapfile = false -- Don't use swapfile
 opt.completeopt = "menu,menuone,noinsert,noselect" -- Autocomplete options
 
@@ -15,25 +15,27 @@ opt.completeopt = "menu,menuone,noinsert,noselect" -- Autocomplete options
 opt.number = true -- Show line number
 opt.relativenumber = true -- Relative line numbers
 opt.showmatch = true -- Highlight matching parenthesis
-opt.foldmethod = "marker" -- Enable folding (default 'foldmarker')
 -- opt.colorcolumn = '90' -- Line lenght marker at 90 columns
 opt.splitright = true -- Vertical split to the right
 opt.splitbelow = true -- Horizontal split to the bottom
-opt.ignorecase = true -- Ignore case letters when search
-opt.smartcase = true -- Ignore lowercase for the whole pattern
 opt.linebreak = true -- Wrap on word boundary
 opt.termguicolors = true -- Enable 24-bit RGB colors
 opt.laststatus = 3 -- Set global statusline
-opt.signcolumn = "yes" -- Signcolumn always on
+opt.signcolumn = "yes" -- Signcolumn always on so no shift happens
 opt.cursorline = true -- Enable highlighting of the current line
 opt.showmode = false -- Don't show mode since we have a statusline
-----------------------------------------------------------
--- Tabs, indent
+opt.smoothscroll = true -- smooth scrolling
+
+-----------------------------------------------------------
+-- Tabs, indent, search
 -----------------------------------------------------------
 opt.expandtab = true -- Use spaces instead of tabs
-opt.shiftwidth = 2 -- Shift 4 spaces when tab
-opt.tabstop = 2 -- 1 tab == 4 spaces
+opt.shiftwidth = 2 -- Shift 2 spaces when tab
+opt.tabstop = 2 -- 1 tab == 2 spaces
 opt.smartindent = true -- Autoindent new lines
+opt.ignorecase = true -- Case insensitive search
+opt.smartcase = true -- Smartly switch back to case sensitive search if needed
+opt.backspace = "indent,eol,start" -- allow bksp on indent, eol or insert mode start position
 
 -----------------------------------------------------------
 -- Memory, CPU
@@ -50,7 +52,15 @@ opt.updatetime = 250 -- ms to wait for trigger an event
 -- Disable nvim intro
 opt.shortmess:append("sI")
 
--- -- Disable builtin plugins
+-----------------------------------------------------------
+-- Folding
+-----------------------------------------------------------
+opt.foldlevel = 99
+opt.foldcolumn = "0"
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- Disable builtin plugins
 local disabled_built_ins = {
 	"2html_plugin",
 	"getscript",
