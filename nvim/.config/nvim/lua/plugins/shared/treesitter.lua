c4ca6650f81252c3eaf123a9809688b81838f631
talama <talama@proton.me>
2024-09-06 18:14:31 +0200

    chore(treesitter): moved to shared plugins folder

--- nvim/.config/nvim/lua/plugins/shared/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/shared/treesitter.lua
@@ -13,59 +13,49 @@ return {
 			-- syntax aware text-objects
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
-		opts = {
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = {
-				"astro",
-				"bash",
-				"c",
-				"cpp",
-				"css",
-				"go",
-				"html",
-				"javascript",
-				"jsdoc",
-				"json",
-				"jsonc",
-				"lua",
-				"luadoc",
-				"markdown",
-				"python",
-				"regex",
-				"scss",
-				"sql",
-				"svelte",
-				"toml",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"xml",
-				"yaml",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"astro",
+					"bash",
+					"c",
+					"cpp",
+					"css",
+					"go",
+					"html",
+					"javascript",
+					"jsdoc",
+					"json",
+					"jsonc",
+					"lua",
+					"luadoc",
+					"markdown",
+					"python",
+					"regex",
+					"scss",
+					"sql",
+					"svelte",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"vue",
+					"xml",
+					"yaml",
 				},
-			},
-		},
-		config = function(_, opts)
-			if type(opts.ensure_installed) == "table" then
-				local added = {}
-				opts.ensure_installed = vim.tbl_filter(function(lang)
-					if added[lang] then
-						return false
-					end
-					added[lang] = true
-					return true
-				end, opts.ensure_installed)
-			end
-			require("nvim-treesitter.configs").setup(opts)
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
 			require("ts_context_commentstring").setup()
 			require("nvim-ts-autotag").setup({
 				opts = {
