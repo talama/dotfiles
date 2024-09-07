425c9ca0696036f08ab0ffb153e73863c72a5b3d
talama <talama@proton.me>
2024-09-07 11:58:30 +0200

    fix(lualin): fixed typo

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -10,7 +10,7 @@ return {
 			},
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = { "branch", icon = "󰘬" },
+				lualine_b = { { "branch", icon = "󰘬" } },
 				lualine_c = {
 					{
 						"diagnostics",
