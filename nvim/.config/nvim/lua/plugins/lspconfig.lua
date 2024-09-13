4a9497ddc223d85f24d5b994e78e73e314db6956
talama <talama@proton.me>
2024-09-13 17:50:48 +0200

    chore(lspconfig): updated lazydev config

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -5,7 +5,15 @@ return {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"hrsh7th/cmp-nvim-lsp",
-		{ "folke/lazydev.nvim", opts = {} },
+		{
+			"folke/lazydev.nvim",
+			opts = {
+				library = {
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				},
+			},
+		},
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
