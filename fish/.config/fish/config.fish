ab9461af6f40f3308b11a2840b0fd9079ebd8c43
talama <talama@proton.me>
2024-10-12 07:34:00 +0200

    feat(fish): updated ssh config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -62,6 +62,22 @@ function test_identities
     end
 end
 
+if [ -n "$SSH_AGENT_PID" ]
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent >/dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    end
+else
+    if [ -f $SSH_ENV ]
+        . $SSH_ENV >/dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent >/dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    else
+        start_agent
+    end
+end
 
 # pnpm
 set -gx PNPM_HOME "/home/talama/.local/share/pnpm"
