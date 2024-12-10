80656e3d4b769ab9c5a51405e5e7189b1bf3c11a
talama <talama@proton.me>
2024-12-10 11:35:10 +0100

    feat(cmp): updated luasnip

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -7,6 +7,7 @@ return {
 			-- Snippet Engine & its associated nvim-cmp source
 			{
 				"L3MON4D3/LuaSnip",
+				version = "v2.*",
 				build = (function()
 					-- Build Step is needed for regex support in snippets.
 					-- This step is not supported in many windows environments.
@@ -21,9 +22,6 @@ return {
 					--    See the README about individual language/framework/plugin snippets:
 					--    https://github.com/rafamadriz/friendly-snippets
 					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
-					end,
 				},
 			},
 			"saadparwaiz1/cmp_luasnip",
@@ -43,6 +41,7 @@ return {
 			local cmp = require("cmp")
 			local lspkind = require("lspkind")
 			local luasnip = require("luasnip")
+			require("luasnip.loaders.from_vscode").lazy_load()
 			luasnip.config.setup({})
 			cmp.setup({
 				snippet = {
