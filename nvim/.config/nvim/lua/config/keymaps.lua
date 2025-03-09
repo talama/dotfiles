06cc9636b8313dfc7cb72a9be8b3078ca73d366e
talama <talama@proton.me>
2025-03-09 18:22:30 +0100

    feat: updated keybindings

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -15,18 +15,19 @@ map("i", "<C-c>", "<Esc>")
 -- quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 
--- new buffer
+-- New buffer
 map("n", "<C-n>", ":enew<CR>", { desc = "New Buffer" })
 
 -- Select All
 -- map("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
 
--- better up/down
+-- Better up/down
 map({ "n", "x" }, "j", "v:count == 2 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 map({ "n", "x" }, "<Down>", "v:count == 2 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 map({ "n", "x" }, "k", "v:count == 2 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 map({ "n", "x" }, "<Up>", "v:count == 2 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
+-- Move line/paragraph
 map("n", "H", "^", { desc = "Beginning of line" })
 map("n", "L", "$", { desc = "End of line" })
 map("n", "J", "}", { desc = "Paragraph Up" })
@@ -53,18 +54,34 @@ map({ "n", "i" }, "<C-b>", "<ESC>^i")
 map({ "n", "i" }, "<C-e>", "<END>")
 
 -- Move Lines
-map({ "n", "v" }, "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-map({ "n", "v" }, "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
--- Tabs
-map("n", "<TAB>", ":bnext<CR>", { desc = "Next Buffer" })
-map("n", "<S-TAB>", ":bprevious<CR>", { desc = "Previous Buffer" })
+-- Buffers
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+
+-- tabs
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
 
 -- Current buffer navigation
 map("n", "<C-u>", "<C-u>zz", { desc = "Move Up" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Move Down" })
+
+-- new file
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
@@ -85,6 +102,8 @@ map("v", ">", ">gv")
 -- comments
 map({ "n", "v" }, "<leader>/", "gcc", { remap = true, desc = "Comment line" })
 map({ "n", "v" }, "<leader>.", "gcb", { remap = true, desc = "Comment block" })
+map("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
+map("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
 
 -- highlights under cursor
 map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
