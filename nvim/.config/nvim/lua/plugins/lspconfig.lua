72eb700c59480ebdd48791f9de5f48a1efc674a9
talama <talama@proton.me>
2025-08-21 20:00:21 +0200

    chore: updated pyright config

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -259,7 +259,10 @@ return {
 				},
 				python = {
 					analysis = {
+						autoSearchPaths = true,
 						typeCheckingMode = "standard",
+						diagnosticMode = "workspace",
+						extraPaths = { "./.venv/lib/python3.x/site-packages" },
 						-- Ignore all files for analysis to exclusively use Ruff for linting
 						-- ignore = { "*" },
 					},
