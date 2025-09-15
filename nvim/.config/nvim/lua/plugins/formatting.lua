0432573e9587e9055d792abebb14268685d45bfe
talama <talama@proton.me>
2025-09-15 11:50:43 +0200

    fix: increased timeout for stylelint

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -68,7 +68,7 @@ return {
 			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 				return
 			end
-			return { timeout_ms = 1000, lsp_format = "fallback" }
+			return { timeout_ms = 2000, lsp_format = "fallback" }
 		end,
 		-- Customize formatters
 		formatters = {},
