f9b97d73b77e34925753273c078404b390b33f31
talama <talama@proton.me>
2025-10-06 20:34:53 +0200

    fix: re-enable dap-python after fix

--- nvim/.config/nvim/lua/plugins/lang/python.lua
+++ nvim/.config/nvim/lua/plugins/lang/python.lua
@@ -7,11 +7,11 @@ end
 return {
 	{
 		"mfussenegger/nvim-dap-python",
-  -- stylua: ignore
-  keys = {
-    { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
-    { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
-  },
+	 -- stylua: ignore
+	 keys = {
+	   { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+	   { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+	 },
 		config = function()
 			if vim.fn.has("win32") == 1 then
 				require("dap-python").setup(get_path("debugpy", "/venv/Scripts/pythonw.exe"))
