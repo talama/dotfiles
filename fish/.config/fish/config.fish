95301092527bdfa3796539038c75a98ad8059e73
talama <talama@proton.me>
2026-06-21 19:04:16 +0200

    fix: updated fish with keychain for ssh

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -35,43 +35,7 @@ alias lla "ll -a"
 alias tm "~/.tmux/tmux-sessionizer.sh"
 
 #auto launch ssh-agent
-setenv SSH_ENV $HOME/.ssh/environment
-
-function start_agent
-    echo "Initializing new SSH agent ..."
-    ssh-agent -c | sed 's/^echo/#echo/' >$SSH_ENV
-    echo succeeded
-    chmod 600 $SSH_ENV
-    . $SSH_ENV >/dev/null
-    ssh-add
-end
-
-function test_identities
-    ssh-add -l | grep "The agent has no identities" >/dev/null
-    if [ $status -eq 0 ]
-        ssh-add
-        if [ $status -eq 2 ]
-            start_agent
-        end
-    end
-end
-
-if [ -n "$SSH_AGENT_PID" ]
-    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent >/dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    end
-else
-    if [ -f $SSH_ENV ]
-        . $SSH_ENV >/dev/null
-    end
-    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent >/dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    else
-        start_agent
-    end
-end
+keychain id_github --quiet --eval | source
 
 # pnpm
 set -gx PNPM_HOME "/home/talama/.local/share/pnpm"
@@ -88,10 +52,6 @@ enable_transience
 
 # Generated for envman. Do not edit.
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
 
 # uv
 fish_add_path "/home/talama/.local/bin"
